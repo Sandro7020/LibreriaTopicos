fae36184398ea704803ff769ad3b95fd289b36db
Sandro7020 <alessandroventresc@gmail.com>
2025-06-05 09:41:07 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+export class Stack<T> {
+    private arr: T[];
+
+    constructor() {
+        this.arr = [];
+    }
+
+    public push(value: T): void {
+        this.arr.push(value);
+    }
+
+    public pop(): T | void {
+        return this.arr.pop();
+    }
+
+    public top(): T | void {
+        if (this.arr.length) return this.arr[this.arr.length - 1];
+    }
+
+    public isVoid(): boolean {
+        return this.arr.length === 0;
+    }
+}
